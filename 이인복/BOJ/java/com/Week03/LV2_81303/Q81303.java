0a8516222c21c2241a118b3d7fc1152b4f147cd9
nashs789 <nashs789@naver.com>
2024-05-28 00:09:06 +0900

    Feat: [LV2_81303] 표 편집 Pass

--- 이인복/BOJ/java/com/Week03/LV2_81303/Q81303.java
+++ 이인복/BOJ/java/com/Week03/LV2_81303/Q81303.java
@@ -17,90 +17,79 @@ public class Q81303 {
 
     static class Solution {
 
-        public static boolean DELETE = true;
-        public static boolean ROLLBACK = false;
-
-        public static int FIRST_ROW = 0;
-        public static int CUR_ROW = Integer.MAX_VALUE;
-        public static int LAST_ROW = Integer.MAX_VALUE;
+        public static char DELETE = 'X';
+        public static char ROLLBACK = 'O';
 
         public String solution(int n, int k, String[] cmd) {
-            boolean[] table = new boolean[n];  // false: 표 존재, true: 표 삭제
+            char[] table = new char[n];  // false: 표 존재, true: 표 삭제
             Stack<Integer> deleted = new Stack<>();
-            CUR_ROW = k;
-            LAST_ROW = n - 1;
+
+            Arrays.fill(table, 'O');
+            int moveCnt = 0;
 
             for(String c : cmd) {
-                String[] cs = Arrays.stream(c.split(" ")).toArray(String[]::new);
+                char op = c.charAt(0);
 
-                if("U".equals(cs[0])) {
-                    up(Integer.parseInt(cs[1]), table);
-                } else if("D".equals(cs[0])) {
-                    down(Integer.parseInt(cs[1]), table);
-                } else if("C".equals(cs[0])) {
-                    delete(deleted, table);
+                if('U' == op) {
+                    moveCnt -= Integer.parseInt(c.substring(2));
+                } else if('D' == op) {
+                    moveCnt += Integer.parseInt(c.substring(2));
                 } else {
+                    k = move(k, moveCnt, table);
+                    moveCnt = 0;
+                }
+
+                if('C' == op) {
+                    k = delete(k, deleted, table);
+                } else if('Z' == op){
                     rollback(deleted, table);
                 }
             }
 
-            StringBuilder sb = new StringBuilder();
-
-            for(boolean row : table) {
-                sb.append(row ? "X" : "O");
-            }
-
-            return sb.toString();
+            return new String(table);
         }
 
-        public static void rollback(Stack<Integer> deleted, boolean[] table) {
-            int rollbackIdx = deleted.pop();
-
-            table[rollbackIdx] = ROLLBACK;
-
-            if(rollbackIdx > LAST_ROW) {
-                LAST_ROW = rollbackIdx;
-            } else if(rollbackIdx < FIRST_ROW) {
-                FIRST_ROW = rollbackIdx;
-            }
+        public static void rollback(Stack<Integer> deleted, char[] table) {
+            table[deleted.pop()] = ROLLBACK;
         }
 
-        public static void delete(Stack<Integer> deleted, boolean[] table) {
-            table[CUR_ROW] = DELETE;
-            deleted.push(CUR_ROW);
+        public static int delete(int k, Stack<Integer> deleted, char[] table) {
+            table[k] = DELETE;  // 삭제
+            deleted.push(k);  // 롤백 준비
 
-            if(CUR_ROW == LAST_ROW) {
-                up(1, table);
-                LAST_ROW = CUR_ROW;
-            } else {
-                boolean firstChange = false;
+            int backup = k;
 
-                if(CUR_ROW == FIRST_ROW) {
-                    firstChange = true;
-                }
-
-                down(1, table);
-
-                if(firstChange) {
-                    FIRST_ROW = CUR_ROW;
-                }
+            while(k < table.length && table[k] == DELETE) {
+                k++;
             }
+
+            if(k < table.length) return k;
+
+            return k == table.length - 1 ? down(backup, 1, table) : up(backup, -1, table);
+        }
+
+        public static int move(int k, int dist, char[] table) {
+            return dist > 0 ? down(k, dist, table) : up(k, dist, table);
         }
         
-        public static void down(int dist, boolean[] table) {
-            if(CUR_ROW == LAST_ROW) return;
-
-            for(int i = 0; i < dist; i++) {
-                while(table[++CUR_ROW]) {}
+        public static int down(int k, int dist, char[] table) {
+            while(dist > 0) {
+                if(table[++k] == ROLLBACK) {
+                    dist--;
+                }
             }
+
+            return k;
         }
 
-        public static void up(int dist, boolean[] table) {
-            if(CUR_ROW == FIRST_ROW) return;
-
-            for(int i = 0; i < dist; i++) {
-                while(table[--CUR_ROW]) {}
+        public static int up(int k, int dist, char[] table) {
+            while(dist < 0) {
+                if(table[--k] == ROLLBACK) {
+                    dist++;
+                }
             }
+
+            return k;
         }
     }
 }
