ac0c1cb7b7427b7fce603fde842c12deff239299
nashs789 <nashs789@naver.com>
2024-08-02 01:10:47 +0900

    Feat: Week11 문제 풀이 with Java

---
+++ 이인복/BOJ/java/com/Week11/LV2_42885/LV2_42885.java
@@ -0,0 +1,36 @@
+package com.Week11.LV2_42885;
+
+import java.util.Arrays;
+
+public class LV2_42885 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(
+            sol.solution(new int[]{70, 50, 80, 50}, 100)
+        );
+    }
+
+    static class Solution {
+        public int solution(int[] people, int limit) {
+            int ans = 0;
+            int l = 0;
+            int r = people.length - 1;
+
+            Arrays.sort(people);
+
+            while(l <= r) {
+                int sum = people[l] + people[r];
+
+                if(sum <= limit) {
+                    l++;
+                }
+
+                r--;
+                ans++;
+            }
+
+            return ans;
+        }
+    }
+}
