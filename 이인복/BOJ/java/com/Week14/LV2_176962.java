9eaefa2f801bd82229f6617c08a49c4086810ce4
nashs789 <nashs789@naver.com>
2024-08-27 01:20:27 +0900

    Feat: LV2_172927 광물 캐기 문제 풀이 with Java

--- 이인복/BOJ/java/com/Week14/LV2_176962.java
+++ 이인복/BOJ/java/com/Week14/LV2_176962.java
@@ -28,12 +28,6 @@ public class LV2_176962 {
         }
     }
 
-    /**
-     * 1. 새로운 과제를 시작할 시각이 되었을 때, 기존에 진행 중이던 과제가 있다면 진행 중이던 과제를 멈추고 새로운 과제를 시작 합니다.
-     * 2. 진행중이던 과제를 끝냈을 때, 잠시 멈춘 과제가 있다면, 멈춰둔 과제를 이어서 진행 합니다.
-     * 3. 과제를 끝낸 시각에 새로 시작해야 되는 과제와 잠시 멈춰둔 과제가 모두 있다면, 새로 시작해야 하는 과제부터 진행합니다.
-     * 4. 멈춰둔 과제가 여러 개일 경우, 가장 최근에 멈춘 과제부터 시작합니다.
-     */
     static class Solution {
         public String[] solution(String[][] plans) {
             Queue<Task> tasks = new PriorityQueue<>((a, b) -> a.srtTime.isAfter(b.srtTime) ? 1 : -1);
---
+++ 이인복/BOJ/java/com/Week15/LV2_172927.java
@@ -0,0 +1,137 @@
+package com.Week15;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LV2_172927 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(
+            sol.solution(
+                    new int[]{1,1,0},
+                    new String[] {"diamond", "diamond", "diamond", "iron", "iron", "diamond", "iron", "stone","iron", "iron", "diamond","diamond"}
+            )
+        );
+    }
+
+    static class Solution {
+        public int solution(int[] picks, String[] minerals) {
+            List<Mass> masses = getSortedMasses(Arrays.stream(picks).sum(), minerals);
+            int ans = 0;
+
+            for(Mass mass : masses) {
+                if(picks[0] != 0) {
+                    ans += mass.getFatigue(25);
+                    picks[0]--;
+                } else if(picks[1] != 0) {
+                    ans += mass.getFatigue(5);
+                    picks[1]--;
+                } else if(picks[2] != 0) {
+                    ans += mass.getFatigue(1);
+                    picks[2]--;
+                } else {
+                    break;
+                }
+            }
+
+            return ans;
+        }
+
+        private static List<Mass> getSortedMasses(int totalPick, String[] minerals) {
+            List<Mass> masses = new ArrayList<>();
+            int cnt = 0;
+            int dia = 0;
+            int iro = 0;
+            int sto = 0;
+
+            for(String mineral : minerals) {
+                if(totalPick == 0) break;
+
+                switch(mineral.charAt(0)) {
+                    case 'd': dia++; break;
+                    case 'i': iro++; break;
+                    case 's': sto++; break;
+                    default: // Unreachable Code
+                }
+
+                cnt++;
+
+                if(cnt == 5) {
+                    masses.add(new Mass(dia, iro, sto));
+                    cnt = 0;
+                    dia = 0;
+                    iro = 0;
+                    sto = 0;
+                    totalPick--;
+                }
+            }
+
+            if(totalPick != 0 && (dia != 0 || iro != 0 || sto != 0)) {
+                masses.add(new Mass(dia, iro, sto));  // 5 개 묶음이 아닌 케이스
+            }
+
+            return masses.stream().sorted(Mass::compareTo).collect(Collectors.toList());
+        }
+    }
+
+    static class Mass implements Comparable<Mass>{
+        private final int DIAMOND = 25;
+        private final int IRON = 5;
+        private final int STONE = 1;
+        int dia;
+        int iro;
+        int sto;
+
+        public Mass(int dia, int iro, int sto) {
+            this.dia = dia;
+            this.iro = iro;
+            this.sto = sto;
+        }
+
+        public int getFatigue(int pick) {
+            return calcEachFatigue(pick, dia, DIAMOND) +
+                   calcEachFatigue(pick, iro, IRON) +
+                   calcEachFatigue(pick, sto, STONE);
+        }
+
+        public int calcEachFatigue(int pick, int mineral, int value) {
+            int sum = 0;
+
+            while(mineral-- > 0) {
+                int res = value / pick;
+                sum += (res == 0) ? 1 : res;
+            }
+
+            return sum;
+        }
+
+        @Override
+        public int compareTo(Mass mass) {
+            int diaRes = mass.dia - this.dia;
+
+            if(diaRes == 0) {
+                int ironRes = mass.iro - this.iro;
+
+                if(ironRes == 0) {
+                    return mass.sto - this.sto;
+                }
+
+                return ironRes;
+            }
+
+            return diaRes;
+        }
+
+        @Override
+        public String toString() {
+            return "Mass{" +
+                    "dia=" + dia +
+                    ", iro=" + iro +
+                    ", sto=" + sto +
+                    '}';
+        }
+    }
+}
