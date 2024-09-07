7fbcba1e7a542db08527ac8fa2adfbe477a33195
CHOIJIHO <wlghsp@gmail.com>
2024-09-07 18:02:33 +0900

    fix: 네트워크 풀이 수정

--- CJH/Week16/네트워크/Solution.java
+++ CJH/Week16/네트워크/Solution.java
@@ -1,45 +1,31 @@
 package programmers.level3.네트워크;
 
-import java.util.ArrayList;
 
 public class Solution {
 
-    ArrayList<ArrayList<Integer>> network;
     boolean[] visited;
+
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        visited = new boolean[n + 1];
-        network = new ArrayList<>();
+        visited = new boolean[n];
 
-        for (int i = 0; i < n + 1; i++) {
-            network.add(new ArrayList<>());
-        }
-        for (int i = 0; i < computers.length; i++) {
-            for (int j = 0; j < computers[0].length; j++) {
-                   if (i != j && computers[i][j] == 1) {
-                       network.get(i + 1).add(j + 1);
-                   }
-            }
-        }
-
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i < n; i++) {
             if (!visited[i]) {
                 answer++;
-                dfs(i);
+                dfs(i, computers);
             }
         }
 
         return answer;
     }
 
-    private void dfs(int start) {
+    private void dfs(int start, int[][] computers) {
         visited[start] = true;
 
-        for (Integer i : network.get(start)) {
-            if (!visited[i]) {
-                dfs(i);
+        for (int i = 0; i < computers.length; i++) {
+            if (!visited[i] && computers[start][i] == 1) {
+                dfs(i, computers);
             }
         }
-
     }
 }
