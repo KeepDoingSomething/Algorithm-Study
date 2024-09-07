418cccf711be96d5da2e7fba334796b3629f04c1
CHOIJIHO <wlghsp@gmail.com>
2024-09-07 18:04:38 +0900

    remove: 기둥과 보 설치 삭제

--- CJH/Week16/기둥과_보_설치/Solution.java
+++
@@ -1,8 +0,0 @@
-package programmers.level3.기둥과_보_설치;
-
-public class Solution {
-    public int[][] solution(int n, int[][] build_frame) {
-        int[][] answer = {};
-        return answer;
-    }
-}
