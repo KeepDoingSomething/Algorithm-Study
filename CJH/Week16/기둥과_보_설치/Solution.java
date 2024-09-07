e02cae49926e914ea983d61e5eab61c09da01912
CHOIJIHO <wlghsp@gmail.com>
2024-09-07 17:45:38 +0900

    feat: Week16 문제풀이

---
+++ CJH/Week16/기둥과_보_설치/Solution.java
@@ -0,0 +1,8 @@
+package programmers.level3.기둥과_보_설치;
+
+public class Solution {
+    public int[][] solution(int n, int[][] build_frame) {
+        int[][] answer = {};
+        return answer;
+    }
+}
