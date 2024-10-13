48d22c9926520cad25926ccaada6bc66629dd115
ygs <ygs3004@naver.com>
2024-10-14 01:48:50 +0900

    [21주차 / 윤건수] 주식가격 (Level 2)

--- 윤건수/programmers/LV2_42584/Solution.java
+++ 윤건수/programmers/LV2_42584/Solution.java
@@ -1,4 +1,22 @@
 package programmers.LV2_42584;
 
 public class Solution {
+
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        for(int i=0; i<prices.length; i++){
+            int currentPrice = prices[i];
+            int period = 0;
+            for(int j=i+1; j<prices.length; j++){
+                int comparePrice = prices[j];
+                period++;
+                if(currentPrice > comparePrice){
+                    break;
+                }
+            }
+            answer[i] = period;
+        }
+        return answer;
+    }
+
 }
