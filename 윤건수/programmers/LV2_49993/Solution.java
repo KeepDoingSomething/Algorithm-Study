9dd05ad4c66b03c5b7990d820ed93e774119dde6
ygs <ygs3004@naver.com>
2024-10-21 21:26:15 +0900

    [22주차 / 윤건수] 스킬트리 (Level 2)

--- 윤건수/programmers/LV2_49993/Solution.java
+++ 윤건수/programmers/LV2_49993/Solution.java
@@ -2,6 +2,33 @@ package programmers.LV2_49993;
 
 public class Solution {
     public int solution(String skill, String[] skill_trees) {
-        return 0;
+
+        String[] skillTree = skill.split("");
+        int answer = 0;
+
+        for(String tree : skill_trees){
+            String[] userTree = tree.split("");
+            int treeIdx = 0;
+            int userIdx = 0;
+
+            while(userIdx < tree.length() && treeIdx < skill.length()){
+                if(skill.contains(userTree[userIdx]) && userTree[userIdx].equals(skillTree[treeIdx])){
+                    userIdx++;
+                    treeIdx++;
+                }else if (skill.contains(userTree[userIdx]) && !userTree[userIdx].equals(skillTree[treeIdx])) {
+                    break;
+                }else{
+                    userIdx++;
+                }
+            }
+
+            if(userIdx >= tree.length() || treeIdx >= skill.length()){
+                answer++;
+            }
+
+        }
+
+        return answer;
     }
+
 }
