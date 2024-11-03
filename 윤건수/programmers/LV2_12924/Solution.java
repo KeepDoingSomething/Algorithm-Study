712d3705456e46d74f51f10d999eb857f1363cdf
ygs <ygs3004@naver.com>
2024-11-03 18:43:34 +0900

    [23주차 / 윤건수] 숫자의 표현 (Level 2)

--- 윤건수/programmers/LV2_12924/Solution.java
+++ 윤건수/programmers/LV2_12924/Solution.java
@@ -2,6 +2,23 @@ package programmers.LV2_12924;
 
 public class Solution {
     public int solution(int n) {
-        return 1;
+        int low = 0;
+        int high = 1;
+        int sum = 1;
+        int answer = 0;
+        while(high <= n){
+            if(sum == n){
+                answer++;
+                high++;
+                sum+=high;
+            } else if (sum >= n){
+                sum -= low;
+                low++;
+            }else {
+                high++;
+                sum += high;
+            }
+        }
+        return answer;
     }
 }
