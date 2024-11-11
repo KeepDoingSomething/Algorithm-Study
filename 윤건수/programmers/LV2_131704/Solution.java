bb7c69c804c7ed3c59fa25dcbec797432e4b4331
ygs <ygs3004@naver.com>
2024-11-11 21:24:25 +0900

    [24주차 / 윤건수] 택배상자 (Level 2)

--- 윤건수/programmers/LV2_131704/Solution.java
+++ 윤건수/programmers/LV2_131704/Solution.java
@@ -1,4 +1,31 @@
 package programmers.LV2_131704;
 
+import java.util.*;
+
 public class Solution {
+
+    public int solution(int[] order) {
+        Stack<Integer> stack = new Stack<>();
+        int oIdx = 0;
+
+        for(int box=1; box<=order.length; box++){
+            boolean isPutNow = false;
+            while(true){
+                if(!isPutNow && order[oIdx] == box){
+                    oIdx++;
+                    isPutNow = true;
+                }else if(!stack.isEmpty() && stack.peek() == order[oIdx]){
+                    stack.pop();
+                    oIdx++;
+                }else{
+                    break;
+                }
+            }
+
+            if(!isPutNow) stack.add(box);
+        }
+
+        return oIdx;
+    }
+
 }
