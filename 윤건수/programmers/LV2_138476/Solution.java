877f6964733d3e6ce5d0e6df2907b46121ee02da
ygs <ygs3004@naver.com>
2024-12-16 22:55:47 +0900

    [29주차 / 윤건수] 귤 고르기 (Level 2)

--- 윤건수/programmers/LV2_138476/Solution.java
+++ 윤건수/programmers/LV2_138476/Solution.java
@@ -1,4 +1,29 @@
 package programmers.LV2_138476;
 
+import java.util.*;
+import java.util.stream.*;
+
 public class Solution {
+    public int solution(int k, int[] tangerine) {
+        Map<Integer, Integer> quantityOfSizePerTangerine = new HashMap<>();
+
+        for(int sizeOfTangerine : tangerine){
+            quantityOfSizePerTangerine.compute(sizeOfTangerine, (key, value) -> value == null ? 1 : value + 1);
+        }
+
+        List<Integer> sameSizeValues = quantityOfSizePerTangerine.values()
+                .stream()
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        int indexPerSize = 0;
+        int total = 0;
+
+        while(total<k){
+            total += sameSizeValues.get(indexPerSize);
+            indexPerSize++;
+        }
+
+        return indexPerSize;
+    }
 }
