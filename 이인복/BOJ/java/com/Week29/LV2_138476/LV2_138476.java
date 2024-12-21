8df6b71d37d2266b896e20ea1a2608ecb0fd42f4
nashs789 <bbock1214@gmail.com>
2024-12-21 13:10:18 +0900

    Feat: LV2 귤 고르기 문제 풀이 with Java

---
+++ 이인복/BOJ/java/com/Week29/LV2_138476/LV2_138476.java
@@ -0,0 +1,32 @@
+package com.Week29.LV2_138476;
+
+import java.util.Arrays;
+import java.util.*;
+import java.util.stream.*;
+
+public class LV2_138476 {
+    public static void main(String[] args) {
+
+    }
+
+    static class Solution {
+        public int solution(int k, int[] tangerine) {
+            int ans = 0;
+            Queue<Long> pq = new PriorityQueue<>((a, b) -> b.compareTo(a));
+            Map<Integer, Long> countMap = Arrays.stream(tangerine)
+                                                .boxed()
+                                                .collect(Collectors.groupingBy(
+                                                        num -> num, Collectors.counting()
+                                                ));
+
+            pq.addAll(countMap.values());
+
+            while(k > 0) {
+                ans++;
+                k -= pq.poll();
+            }
+
+            return ans;
+        }
+    }
+}
